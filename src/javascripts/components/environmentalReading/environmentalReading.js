81f124c6782cef37e2cb5e517361ea126a19bdd9
Michele Rawlins <shellbop@att.net>
2020-04-15 21:32:47 -0500

    added navbar

--- src/javascripts/components/environmentalReading/environmentalReading.js
+++ src/javascripts/components/environmentalReading/environmentalReading.js
@@ -1,39 +1,23 @@
 
 import axios from 'axios';
-import apiKeys from '../apiKeys.json';
+// import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
-const getCows = () => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/cows.json`)
+const getEnvironmentalData = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/envReadings.json`)
     .then((response) => {
-      const demCows = response.data;
-      const cows = [];
-      if (demCows) {
-        Object.keys(demCows).forEach((cowId) => {
-          demCows[cowId].id = cowId;
-          cows.push(demCows[cowId]);
+      const demEnviroData = response.data;
+      const enviroData = [];
+      if (demEnviroData) {
+        Object.keys(demEnviroData).forEach((enviroId) => {
+          demEnviroData[enviroId].id = enviroId;
+          enviroData.push(demEnviroData[enviroId]);
         });
       }
-
-      resolve(cows);
+      resolve(enviroData);
     })
     .catch((err) => reject(err));
 });
 
-const getEnvironmentalData = () => new Promise((resolve, reject) => {
-  axios.get(${baseUrl}/environmentalSeedData.json)
-  .then((response) => {
-  const demEnviroData = response.data;
-  const enviroData = [];
-  if (demEnviroData) {
-  Object.keys(demEnviroData).forEach((enviroId) => {
-  demEnviroData[enviroId].id = enviroId;
-  enviroData.push(demEnviroData[enviroId]);
-  });
-  }
-  
-    resolve(enviroData);
-  })
-  .catch((err) => reject(err));
-  });
+export default { getEnvironmentalData };
