e68fa35bb7aae82b28b8895b201bf820fd0c0c49
Anca Simon <ancarsimon@gmail.com>
2020-04-16 14:30:52 -0500

    wip

--- src/javascripts/components/destinationCard/destinationCard.js
+++ src/javascripts/components/destinationCard/destinationCard.js
@@ -1,11 +1,15 @@
 const buildDestinationCard = (destination) => {
   let domString = '';
-  domString += '<div class="card" style="width: 18rem;">';
-  domString += '<img src="..." class="card-img-top" alt="...">';
+  domString += '<div class="card cardDisplay" style="width: 18rem;">';
+  domString += `<img src="${destination.imageUrl}" class="card-img-top" alt="${destination.alt}">`;
   domString += '<div class="card-body">';
   domString += `<h5 class="card-title">${destination.name}</h5>`;
   domString += '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>';
-  domString += '<a href="#" class="btn btn-primary">Go somewhere</a>';
+  // domString += '<button type="button" class="btn btn-primary crudButtonImage">Go somewhere</button>';
+  domString += '<div class="row justify-content-around">';
+  domString += '<button type="button" class="col-4 btn-default btn-lg crudButtonImage glowing"><i class="fas fa-feather-alt"></i></button>';
+  domString += '<button type="button" class="col-4 btn-default btn-lg crudButtonImage glowing"><i class="fas fa-skull-crossbones"></i></button>';
+  domString += '</div>';
   domString += '</div>';
   domString += '</div>';
 
--- src/javascripts/components/destinationsContainer/destinationsContainer.js
+++ src/javascripts/components/destinationsContainer/destinationsContainer.js
@@ -6,9 +6,19 @@ const buildDestinationsContainer = () => {
   destinationData.getDestinations()
     .then((destinations) => {
       let domString = '';
+      domString += '<div class="pageDisplay">';
+      domString += '<h1 class="headingDisplay softEmboss"><p class="typewriter">Destinations</p></h1>';
+      domString += '<div class="text-center m-5">';
+      domString += '<input type="button" class="col-4 btn-default btn-lg crudButtonImage glowing" value="Add a New Destination">';
+      domString += '</div>';
+      domString += '<div class="d-flex flex-wrap">';
       destinations.forEach((item) => {
+        domString += '<div class="col-4">';
         domString += destinationCard.buildDestinationCard(item);
+        domString += '</div>';
       });
+      domString += '</div>';
+      domString += '</div>';
       utils.printToDom('destinations', domString);
     })
     .catch((error) => console.error('get destinations broke', error));
