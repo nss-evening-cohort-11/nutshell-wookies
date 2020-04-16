7c560b839e2c4e4738722e44952d1a51410cbd0c
Michele Rawlins <shellbop@att.net>
2020-04-16 18:43:49 -0500

    added envReadings

---
+++ src/javascripts/components/enviromentalReadings/enviromentalReadings.js
@@ -0,0 +1,48 @@
+const envReadingMaker = (envReading) => {
+  let domstring = "";
+  <div class="row">
+    <div class="col-sm-9">
+      <img src = "$`{envReading.Url}`
+      <div class="row">
+      <div class="col-sm-9">
+      Level 2: .col-sm-9
+        <div class="col-8 col-sm-6">
+          Level 2: .col-8 .col-sm-6
+        </div>
+        <div class="col-4 col-sm-6">
+          Level 2: .col-4 .col-sm-6
+        </div><div class="col-8 col-sm-6">
+          Level 2: .col-8 .col-sm-6
+        </div>
+        <div class="col-4 col-sm-6">
+          Level 2: .col-4 .col-sm-6
+        </div>
+        <div class="col-8 col-sm-6">
+          Level 2: .col-8 .col-sm-6
+        </div>
+        <div class="col-4 col-sm-6">
+          Level 2: .col-4 .col-sm-6
+        </div>
+        <div class="col-8 col-sm-6">
+          Level 2: .col-8 .col-sm-6
+        </div>
+        <div class="col-4 col-sm-6">
+          Level 2: .col-4 .col-sm-6
+        </div>
+        <div class="col-8 col-sm-6">
+          Level 2: .col-8 .col-sm-6
+        </div>
+        <div class="col-4 col-sm-6">
+          Level 2: .col-4 .col-sm-6
+        </div>
+        <div class="col-8 col-sm-6">
+          Level 2: .col-8 .col-sm-6
+        </div>
+        <div class="col-4 col-sm-6">
+          Level 2: .col-4 .col-sm-6
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  }
--- src/javascripts/helpers/data/envReadings.js
+++ src/javascripts/helpers/data/envReadings.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-// import apiKeys from '../apiKeys.json';
+import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
