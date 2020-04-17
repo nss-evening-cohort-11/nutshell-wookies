b7bd2ac7812e69532b4f5ffaa4d8ffaac7e0e2eb
Anca Simon <ancarsimon@gmail.com>
2020-04-17 15:26:26 -0500

    finished auth and login and logout features

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,22 +1,24 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import destinationsContainer from '../../components/destinationsContainer/destinationsContainer';
 
 // const dashboardDiv = $('#dashboard');
 // const crewDiv = $('#crew');
 // const destinationsDiv = $('#destinations');
 // const envReadingsDiv = $('#envReadings');
 // const speciesDiv = $('#species');
-// const logoutButton = $('#navbar-logout-button');
+const loginButton = $('#navbar-login-button');
+const logoutButton = $('#navbar-logout-button');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      // person is logged in
-      destinationsContainer.buildDestinationsContainer();
+      loginButton.addClass('hide');
+      logoutButton.removeClass('hide');
+      $('.crudButtonImage').removeClass('hide');
     } else {
-      // person is not logged in
-      destinationsContainer.buildDestinationsContainer();
+      loginButton.removeClass('hide');
+      logoutButton.addClass('hide');
+      $('.crudButtonImage').addClass('hide');
     }
   });
 };
