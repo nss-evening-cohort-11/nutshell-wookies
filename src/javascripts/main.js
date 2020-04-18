bf4b7df7eda3c2b7db81a363d2ad8913bd14d874
Anca Simon <ancarsimon@gmail.com>
2020-04-18 09:30:56 -0500

    minor auth cap correction

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,7 +4,7 @@ import authData from './helpers/data/authData';
 import logoutButton from './components/logout/logout';
 import destinationsContainer from './components/destinationsContainer/destinationsContainer';
 import speciesContainer from './components/SpeciesContainer/speciesContainer';
-import authActions from './components/Auth/auth';
+import authActions from './components/auth/auth';
 import '../styles/main.scss';
 
 import envReading from './components/enviromentalContainer/enviromentalContainer';
