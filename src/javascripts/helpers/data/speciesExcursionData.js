f378c452d5385f8b230fdda73f02ab72501f274f
Kayla Melton <kannmelton2@gmail.com>
2020-05-02 12:42:22 -0500

    changed where if statment ends in speciesExcursionData file

--- src/javascripts/helpers/data/speciesExcursionData.js
+++ src/javascripts/helpers/data/speciesExcursionData.js
@@ -13,8 +13,8 @@ const getSpeciesExcursionbyExcursionId = (excursionId) => new Promise((resolve, 
           allSpeciesExcursions[speciesExcursionId].id = speciesExcursionId;
           speciesExcursions.push(allSpeciesExcursions[speciesExcursionId]);
         });
-        resolve(speciesExcursions);
       }
+      resolve(speciesExcursions);
     })
     .catch((err) => reject(err));
 });
