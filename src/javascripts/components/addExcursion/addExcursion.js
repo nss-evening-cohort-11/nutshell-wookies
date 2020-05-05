5083f5df9c6bacce8f1449cd3af92deab7390acc
Beth <bethhnielsen56@gmail.com>
2020-05-05 18:47:25 -0500

    inputs added to form

--- src/javascripts/components/addExcursion/addExcursion.js
+++ src/javascripts/components/addExcursion/addExcursion.js
@@ -4,12 +4,20 @@ const newExcursionForm = () => {
   let domString = '';
   domString += '<form>';
   domString += '<div class="form-group">';
-  domString += '<label for="new-excursion-name">Name</label>';
+  domString += '<label for="new-excursion-name">Excursion Name</label>';
   domString += '<input type="text" class="form-control" id="new-excursion-name" placeholder="Swimming with Dolphins">';
   domString += '</div>';
   domString += '<div class="form-group">';
-  domString += '<label for="new-excursion-date">Date</label>';
+  domString += '<label for="new-excursion-date">Excursion Date</label>';
   domString += '<input type="date" class="form-control" id="new-excursion-date" value="2020-05-30" min="2020-05-15" max="2021-01-31">';
+  domString += '</div>';
+  domString += '<div class="form-group">';
+  domString += '<label for="excursionDestination">Destination Name</label>';
+  domString += '<input type="text" class="form-control" id="excursionDestination" placeholder="Hawaii">';
+  domString += '</div>';
+  domString += '<div class="form-group">';
+  domString += '<label for="excursionDestinationImage">Destination Image</label>';
+  domString += '<input type="text" class="form-control" id="excursionDestinationImage" placeholder="Image Url">';
   domString += '</div>';
   domString += '</form>';
 
